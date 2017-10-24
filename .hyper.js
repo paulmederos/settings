b21c32ac67e8ef1f31ae2e4aa8b814ce0cc97947
Paul Mederos <pvm@enchant.co>
2017-10-23 20:56:17 -0700

    Updated hyper.js... maybe
    Updating before a wipe of Territory's MBP

--- .hyper.js
+++ .hyper.js
@@ -113,7 +113,7 @@ module.exports = {
   //   `@company/project`
   //   `project#1.0.1`
   plugins: [
-    'hyper-statusline'
+    'hyper-statusline',
   ],
 
   // in development, you can create a directory under
